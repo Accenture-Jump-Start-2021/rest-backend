e57b592007bd6a697b44f8bcb26b3ac2ac940856
daniel.hardjono <daniel.hardjono@amac02dn8hjmd6m.speedport.ip>
2021-03-03 10:40:12 +0100

    added trainers route

---
+++ routes/trainers/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const loki = require('lokijs');
+const axios = require('axios');
+
+const app = express();
+const router = express.Router();
+const pokedexRemoteUrl = 'https://pokeapi.co/api/v2/pokemon/';
+
+
+var db = new loki('trainers.db');
+
+
+// Add a collection to the database
+var trainers = db.addCollection('trainers');
+
+router.post('/', async(req, res) => {
+    trainers.insert(req.body);
+    res.status(201).send(req.body);
+});
+
+router.get('/', async(req, res) => {
+    res.send(trainers.data);
+});
+
+router.get('/:id', async(req, res) => {
+    var id = req.params.id;
+    var result = trainers.findOne({'id': id});
+    if (result) {
+        res.send(result)
+    } else {
+        res.status(404).send(`Trainer with id ${id} not found`)
+    }
+});
+
+router.get('/:id/starter-pokemon', async(req, res) => {
+    var id = req.params.id;
+    var trainer = trainers.findOne({'id': id});
+
+    if (trainer) {
+        axios.get(`${pokedexRemoteUrl}${pokeid}`).then(poke => {
+            console.log('starter pokemon: ', poke.data.name);
+            res.send(poke.data.name)
+        });
+    }
+})
+
+module.exports = router;
+
