e90ea477739733b7ed514a3d8e19da3328cc078e
Martin Jaskulla <martin.jaskulla@accenture.com>
2021-03-03 15:07:06 +0100

    Solve tasks

--- index.js
+++ index.js
@@ -1,34 +1,26 @@
 const express = require('express');
 const pokemonRoute = require('./routes/pokemon');
-const trainersRoute = require('./routes/trainers');
+const axios = require('axios');
 
 const PORT = '3000';
 const app = express();
 
 app.use(express.json());
 app.use('/pokemon', pokemonRoute);
-app.use('/trainers', trainersRoute);
-
-
-app.all('/', (req, res, next) => {
-    console.log('Here');
-    next();
-});
 
 app.get('/', (req, res) => {
-    res.send('Get request successful');
+    res.send('Hello world');
 });
 
-app.get('/greater-than-five/:num', (req, res) => {
-    var num = req.params.num;
-    if (num > 5) {
-        res.send('success');
-    } else {
-        res.status(401).send('Not authorized');
-    }
+app.post('/', (req, res) => {
+    res.send(req.body);
 });
 
+app.get('/pokedex/:id', async (req, res) => {
+    const id = req.params.id;
+    const response = await axios.get('https://pokeapi.co/api/v2/pokemon/' + id)
+    res.send(response.data.name);
 
-app.listen(PORT, () => {
-    console.log(`Node listening on port ${PORT}`);
 });
+
+app.listen(PORT, () => console.log(`Node listening on port ${PORT}`));
--- routes/pokemon/index.js
+++ routes/pokemon/index.js
@@ -1,71 +1,27 @@
 const express = require('express');
 const loki = require('lokijs');
-const axios = require('axios');
 
-const app = express();
 const router = express.Router();
 const pokedexRemoteUrl = 'https://pokeapi.co/api/v2/pokemon/';
 
-
 var db = new loki('sandbox.db');
 
-
 // Add a collection to the database
-var pokemon = db.addCollection('pokemon');
-
+const pokemonCollection = db.addCollection('pokemon');
 
 router.post('/', (req, res) => {
-    pokemon.insert(req.body);
+    pokemonCollection.insert(req.body);
     res.status(201).send(req.body);
 });
 
-router.get('/', (req, res) => {
-    var poke = pokemon.data;
-    res.send(poke);
-});
-
-
 router.get('/:pokeid', (req, res) => {
-    var pokeid = req.params.pokeid;
-    axios.get(`${pokedexRemoteUrl}${pokeid}`).then( response => {
-        if (response) {
-            res.send(response.data);
-        }
-    }).catch(error => {
-        console.log(error);
-        res.status(500).send(error);
-    })
+    const id = req.params.pokeid;
+    const pokemon = pokemonCollection.find({pokeId: id});
+    if(pokemon.length > 0) {
+        res.send(pokemon)
+    } else {
+        res.status(404).send(`Pokemon with id ${id} not found`)
+    }
 });
-
-router.put('/:pokeid', (req, res) => {
-    var pokeid = req.params.pokeid;
-    var poke = pokemon.findOne({'pokeid': pokeid});
-
-    if (poke) {
-        for (let item in req.body) {
-            poke[item] = req.body[item]
-        };
-    
-        pokemon.update(poke);
-    
-        res.send(poke);
-    } else {
-        res.status(404).send(`Pokemon with Poke-ID ${pokeid} not found!`);
-    }
-})
-
-
-router.delete('/:pokeid', (req, res) => {
-    var pokeid = req.params.pokeid;
-    var poke = pokemon.findOne({'pokeid': pokeid});
-
-    if (poke) {
-        pokemon.remove(poke)
-        res.status(204).send();
-    } else {
-        res.status(404).send(`Pokemon with Poke-ID ${pokeid} not found!`);
-    }
-})
-
 
 module.exports = router;
